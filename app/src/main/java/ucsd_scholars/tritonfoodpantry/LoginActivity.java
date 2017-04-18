9bdad72cffa60e1cebd6311e20cc43344ba60851
ddl012 <ddl012@ucsd.edu>
2017-04-18 16:29:30 -0700

    created sample gui for title screen and login screen

---
+++ app/src/main/java/ucsd_scholars/tritonfoodpantry/LoginActivity.java
@@ -0,0 +1,18 @@
+package ucsd_scholars.tritonfoodpantry;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void login(View view){
+
+    }
+}
---
+++ app/src/main/java/ucsd_scholars/tritonfoodpantry/MainActivity.java
@@ -0,0 +1,20 @@
+package ucsd_scholars.tritonfoodpantry;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickLogin(View view){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+}
